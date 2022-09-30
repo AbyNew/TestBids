5f6ecb10176fae97889dc4e6b672b456a39815f1
abyNew <by_amir@hotmail.com>
2022-09-30 16:05:16 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+let buyers = [
+  {
+      "name": "buyerA",
+      "bids": [ 110, 130]
+  },
+  {
+      "name": "buyerB",
+      "bids": []
+  },
+  {
+      "name": "buyerC",
+      "bids": [ 125]
+  },
+  {
+      "name": "buyerD",
+      "bids": [ 105, 115, 90]
+  },
+  {
+      "name": "buyerE",
+      "bids": [ 132, 135, 140]
+  }];
+  let reservePrice = 100;
+  let  [result, setResult] = useState('');
+
+function lunchTest(e){
+console.log("toto")
+if (!buyers || buyers.length===0){
+  setResult("pas d'enchéres !"); 
+  return result;
+}else{
+  //on a besoin de trouver les meilleures enchéres 
+  let sortedBuyers =[];
+  sortedBuyers = buyers.map(buyer => {
+      let topBids ={
+          buyerName: buyer.name,
+          bestBid: Math.max(...buyer.bids),
+      }
+  return topBids;
+  });        
+  //On a besoin de classer le resultat par ordre croissant     
+  sortedBuyers.sort((a, b) => b.bestBid - a.bestBid);
+
+  //Si le premier résultat dans le tableau est supérieur ou égal au prix de reserve on a notre gagnant.
+  if(sortedBuyers[0].bestBid>=reservePrice){
+  //on a notre gagnant qui est le premier resultat dans le tableau et le prix qui est le deuxiéme ! 
+      setResult("L'enchérisseur "+sortedBuyers[0].buyerName+" remporte l'enchére au prix de "+sortedBuyers[1].bestBid+"€.");
+  }
+  return result;
+}
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Exercice de conception developpement 
+        <button onClick={(e)=>lunchTest(e)}>Lancer le test</button>
+      </div>
+      <div>{result.length > 0 && <h2>{result}</h2>}</div>
     </div>
   );
 }
